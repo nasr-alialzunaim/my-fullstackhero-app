2512723214d4f214eeba7426161bca6e89ab1a0c
Nasraldain Ali Al-zunaim <nasrbussnis22@gmail.com>
2026-08-29 21:16:38 +0300

    p0: remove tenant from confirm email command

--- src/Modules/Identity/Modules.Identity.Contracts/v1/Users/ConfirmEmail/ConfirmEmailCommand.cs
+++ src/Modules/Identity/Modules.Identity.Contracts/v1/Users/ConfirmEmail/ConfirmEmailCommand.cs
@@ -2,4 +2,4 @@ using Mediator;
 
 namespace FSH.Modules.Identity.Contracts.v1.Users.ConfirmEmail;
 
-public sealed record ConfirmEmailCommand(string UserId, string Code, string Tenant) : ICommand<string>;
+public sealed record ConfirmEmailCommand(string UserId, string Code) : ICommand<string>;
